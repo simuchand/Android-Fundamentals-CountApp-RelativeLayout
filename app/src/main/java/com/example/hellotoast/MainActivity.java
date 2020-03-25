bbbad8db0837b32003ee06b2c28ce167b3d1e3f9
Simuchand <simuchand.balachandran@gmail.com>
2020-03-26 00:39:33 +0530

    Hello Toast

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -9,14 +9,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mcount=0;
-    private TextView mShowCount;
+    private int start_count=0, prv_count=0;
+    private TextView start_ShowCount,prv_ShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = findViewById(R.id.show_count);
+        start_ShowCount = findViewById(R.id.show_count);
+        prv_ShowCount = findViewById(R.id.show_count_prv);
     }
 
     public void showToast(View view) {
@@ -25,21 +26,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-        mcount++;
-        if(mShowCount != null)
-            mShowCount.setText(Integer.toString(mcount));
+        start_count++;
+         prv_count=start_count;
+        if(start_ShowCount != null){
+            start_ShowCount.setText(Integer.toString(start_count));
+        }
+
     }
 
     public void countDown(View view) {
-        if(mcount == 0){
+        if(start_count == 0){
+            prv_ShowCount.setText("0");
             Toast toast1 = Toast.makeText(this,R.string.toast_start, Toast.LENGTH_SHORT);
             toast1.show();
         }
-        else if(mShowCount != null){
-            mcount=0;
+        else if(start_ShowCount != null){
+            start_count=0;
             Toast toast1 = Toast.makeText(this,R.string.toast_reset, Toast.LENGTH_SHORT);
             toast1.show();
-            mShowCount.setText("0");
+            prv_ShowCount.setText(Integer.toString(prv_count));
+            start_ShowCount.setText("0");
         }
 
 
